4171ff348d4e5a5a92c62182ac67e4f30620cfc7
emittman <emittman@iastate.edu>
2016-10-26 21:50:21 -0500

    ok

--- src/mat_mult.cu
+++ src/mat_mult.cu
@@ -2,7 +2,7 @@
 
 // Multiply the arrays A and B on GPU and save the result in C
 // C(m,n) = A(m,k) * B(k,n)
-void gpu_blas_mmult(const float *A, const float *B, float *C, const int m, const int k, const int n) {
+void gpu_blas_mmult(const double *A, const double *B, double *C, const int m, const int k, const int n) {
   int lda=m,ldb=k,ldc=m;
   const float alf = 1;
   const float bet = 0;
@@ -12,7 +12,7 @@ void gpu_blas_mmult(const float *A, const float *B, float *C, const int m, const
   cublasHandle_t handle;
   cublasCreate(&handle);
   // Do the actual multiplication
-  cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
+  cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
   // Destroy the handle
   cublasDestroy(handle);
 }
